8b799fb524f3a5b900614a9eda54a355d24205cd
Erebus1678 <dmitryi.platov@gmail.com>
2025-04-10 16:40:11 +0300

    feat: update package.json and index.ts for improved development experience

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,36 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Web3 + TypeScript!');
+app.use(express.json())
+
+app.use((req: Request, res: Response, next: Function) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
+
+app.get('/about', (req: Request, res: Response) => {
+  res.send('Это учебное приложение Web3 на базе Express + TypeScript.');
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  const newUser = req.body;
+  // Здесь можно добавить проверку данных, по желанию.
+  res.status(201).json({ message: 'Пользователь создан', user: newUser });
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  res.send({
+    params: req.params,
+    query:req.query
+  })
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.use((err: any, req: Request, res: Response, next: Function) => {
+  console.error('Ошибка:', err);
+  res.status(500).json({ error: 'Что-то пошло не так!' });
 });
